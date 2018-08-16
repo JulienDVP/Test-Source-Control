c6ca559abf913715045199eed88f2e935aace379
Julien <julien@macbook-pro-de-julien.local>
2018-08-16 12:22:52 +0200

    Ajout classe Test

---
+++ Test du Source Control/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Test du Source Control
+//
+//  Created by Julien on 16/08/2018.
+//  Copyright © 2018 Julien. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+    func salut() {
+        print("Salut les copains")
+    }
+}
